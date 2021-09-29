52e070e6934994beb7690150897e5b8251d83bb1
ryuga <ryuga.private@gmail.com>
2021-09-29 15:56:22 +0900

    fixed ease, easeVector values

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,46 +1,69 @@
 import { gsap } from 'gsap';
 
+
+/**
+* アニメーションさせるオブジェクトに関するクラス
+*/
 class AnimateObject {
   // 対象のオブジェクト
   target: Element | null;
-  // 変位のスピード
+  // easingの具合(どの程度緩やかにmoveさせるか)
   ease: number;
-  // スピード
-  easeMove: number;
+  // 変位のベクトル
+  easeVector: number;
 
   // マウスの基準点の座標
-  mouseX: number = 0;
-  mouseY: number = 0;
+  mouseX = 0;
+  mouseY = 0;
 
   // マウスの現在の座標
-  currentX: number = 0;
-  currentY: number = 0;
+  currentX = 0;
+  currentY = 0;
 
-  constructor(_target: string, _ease: number, _easeMove: number) {
+  /**
+  * アニメーションさせる対象とeasingの設定
+  * @param {string} _target アニメーションさせる対象のクラス
+  * @param {number} _ease easingの度合いを表す値
+  * @param {number} _easeVector 変位のベクトル
+  */
+  constructor(
+    _target: string,
+    _ease: number,
+    _easeVector: number
+  ) {
     this.target = document.querySelector(_target);
     this.ease = _ease;
-    this.easeMove = _easeMove;
+    this.easeVector = _easeVector;
   }
 
-  // 元々マウスの左上がマウスの原点になっているため、カーソルの原点をウィンドウの中心にする
-  mouseMove = (e) => {
+  /**
+  * カーソルの原点をウィンドウの中心にする(元々は左上)
+  * @param {MouseEvent} e イベントハンドラ
+  */
+  mouseMove = (e:MouseEvent) => {
     this.mouseX = e.clientX - (window.innerWidth / 2);
     this.mouseY = e.clientY - (window.innerHeight / 2);
   }
 
-  doAnimate(){
+  /**
+  * アニメーションをさせる
+  */
+  doAnimate = () => {
     document.addEventListener('mousemove', this.mouseMove);
     window.requestAnimationFrame(this.tick);
   }
 
+  /**
+  * 再描画するためのメソッド
+  */
   tick = () => {
-    //カーソルの座標(現在位置に動いた距離を加算して座標に反映している。)
+    //カーソルの座標(現在位置に動いた距離を加算して座標に反映)
     this.currentX += (this.mouseX - this.currentX) * this.ease;
     this.currentY += (this.mouseY - this.currentY) * this.ease;
     gsap.set(this.target, {
       //オブジェクトの座標(カーソルが動いた後の座標を基点としている。)
-      x: this.currentX * this.easeMove,
-      y: this.currentY * this.easeMove,
+      x: this.currentX * this.easeVector,
+      y: this.currentY * this.easeVector,
     });
     window.requestAnimationFrame(this.tick);
   }
@@ -48,23 +71,23 @@ class AnimateObject {
 
 }
 
-const object1 = new AnimateObject('.object1', 0.1, 0);
-object1.doAnimate();
-const object2 = new AnimateObject('.object2', 0.1, -0.05);
-object2.doAnimate();
-const object3 = new AnimateObject('.object3', 0.1, -0.06);
-object3.doAnimate();
-const object4 = new AnimateObject('.object4', 0.1, -0.08);
-object4.doAnimate();
-const object5 = new AnimateObject('.object5', 0.1, -0.15);
-object5.doAnimate();
-const object6 = new AnimateObject('.object6', 0.1, -0.2);
-object6.doAnimate();
-const object7 = new AnimateObject('.object7', 0.1, -0.08);
-object7.doAnimate();
-const object8 = new AnimateObject('.object8', 0.1, -0.1);
-object8.doAnimate();
-const object9 = new AnimateObject('.object9', 0.1, -0.09);
-object9.doAnimate();
-const object10 = new AnimateObject('.object10', 0.1, -0.07);
-object10.doAnimate();
+const city = new AnimateObject('.city', 0.1, 0);
+city.doAnimate();
+const drone = new AnimateObject('.drone', 0.1, 0.03);
+drone.doAnimate();
+const online = new AnimateObject('.online', 0.1, 0.04);
+online.doAnimate();
+const money = new AnimateObject('.money', 0.1, 0.03);
+money.doAnimate();
+const financial = new AnimateObject('.financial', 0.1, -0.12);
+financial.doAnimate();
+const vr = new AnimateObject('.vr', 0.07, -0.2);
+vr.doAnimate();
+const mobile = new AnimateObject('.mobile', 0.1, -0.08);
+mobile.doAnimate();
+const error = new AnimateObject('.error', 0.1, -0.06);
+error.doAnimate();
+const calender = new AnimateObject('.calender', 0.1, 0.03);
+calender.doAnimate();
+const social = new AnimateObject('.social', 0.1, -0.05);
+social.doAnimate();
